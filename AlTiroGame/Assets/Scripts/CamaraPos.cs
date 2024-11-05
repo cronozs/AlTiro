16efcb69d2c7e3960d5c1b14d507e35dfbe01f09
charly <crono_zs@hotmail.com>
2024-11-05 10:50:03 -0600

    arreglando el script de la camara

--- AlTiroGame/Assets/Scripts/CamaraPos.cs
+++ AlTiroGame/Assets/Scripts/CamaraPos.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class CamaraPos : MonoBehaviour
 {
+    [Header("Valores de las camaras, posiciones y numero de jugadores")]
     [Tooltip("en esta variable se guardan los tamaños de camara que va a ocupar la camara cuando solo es un jugador marcado por (ancho)")]
     [SerializeField] private float onePlayerPos;
     [Tooltip("en esta variable se guardan los tamaños de camara que va a ocupar la camara cuando hay dos jugadores marcado por (ancho player 1, ancho player 2)")]
     [SerializeField] private float[] twoPlayersPos;
     [Tooltip("esta variable nos indica si se juega un solo jugador o dos jugadores")]
-    [SerializeField] private bool isTwoPlayers;
-
+    public bool isTwoPlayers;
+    [Header("Referencias a las camaras de los jugadores")]
     [Tooltip("esta variable es para manipular la camara del primer jugador/main camara")]
     [SerializeField] private Camera player1;
     [Tooltip("esta variable es para manipular la camara del segundo jugador")]
     [SerializeField] private Camera player2;
 
-    // Start is called before the first frame update
     void Start()
     {
         if(isTwoPlayers)
@@ -31,11 +31,5 @@ public class CamaraPos : MonoBehaviour
             player2.gameObject.SetActive(false);
             player1.rect = new Rect(onePlayerPos, 0, 1, 1);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
