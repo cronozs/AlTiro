fecc03e5a6db0335eeee3232f25da47160df3327
charly <crono_zs@hotmail.com>
2024-11-06 02:10:56 -0600

    agregando y puliendo el sistema de juego

--- AlTiroGame/Assets/Scripts/Obstacle.cs
+++ AlTiroGame/Assets/Scripts/Obstacle.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     private float _health = 5;
-
+ 
     private void OnTriggerEnter(Collider other)
     {
+        Damage currentDamage = other.GetComponent<Damage>();
+        _health -= currentDamage.damage;
+        Destroy(other.gameObject);
         if(_health <=0)
         {
             Death();
@@ -15,6 +18,7 @@ public class Obstacle : MonoBehaviour
     }
     private void Death()
     {
+        _health = 5;
         this.gameObject.SetActive(false);
     }
 }
