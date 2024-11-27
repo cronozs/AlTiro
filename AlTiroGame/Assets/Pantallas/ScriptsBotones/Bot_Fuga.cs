3ec92839cc24ae342fb02eff17862f5810aef603
Jorge Salas <cdmx2767@amerike.edu.mx>
2024-11-26 22:58:24 -0600

    Escena de inicio y final terminada

---
+++ AlTiroGame/Assets/Pantallas/ScriptsBotones/Bot_Fuga.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bot_Fuga : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
