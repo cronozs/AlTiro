b31798501ba6c455af948c9f1f3658da54778b43
Jorge Salas <cdmx2767@amerike.edu.mx>
2024-12-02 17:11:28 -0600

    creando los modelos para la seleccion de personajes

--- AlTiroGame/Assets/JuegoFinal/ScriptsTodos/Scripts/VidaPlayer1.cs
+++ AlTiroGame/Assets/JuegoFinal/ScriptsTodos/Scripts/VidaPlayer1.cs
@@ -18,32 +18,7 @@ public class VidaPlayer1 : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Botella botella = other.gameObject.GetComponent<Botella>();
-        Mancuerna mancuerna = other.gameObject.GetComponent<Mancuerna>();
-        Chancla chancla = other.gameObject.GetComponent<Chancla>();
-        Cuete cuete = other.gameObject.GetComponent<Cuete>();
         Damage damage = other.gameObject.GetComponent<Damage>();
-
-        if (botella != null)
-        {
-            Destroy(botella.gameObject);
-            VidaActual -= botella.Daño;
-        }
-        else if (mancuerna != null)
-        {
-            Destroy(mancuerna.gameObject);
-            VidaActual -= mancuerna.Daño;
-        }
-        else if (chancla != null)
-        {
-            Destroy(chancla.gameObject);
-            VidaActual -= chancla.Daño;
-        }
-        else if (cuete != null)
-        {
-            Destroy(cuete.gameObject);
-            VidaActual -= cuete.Daño;
-        }
 
         VidaActual -= damage.damage;
 
